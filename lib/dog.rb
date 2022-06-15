8206379bb34fbca8cefdb0c9d3c96b10891cf262
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-06-14 19:03:09 -0600

    commit tree 8b8dea21126f19a9815345164b8bd20ae81bfad6

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,13 @@
-class Dog
+# require "active_record"
+
+# sql = <<-SQL
+#     CREATE TABLE IF NOT EXISTS dogs (
+#         id INTEGER PRIMARY KEY,
+#         name TEXT
+#     )
+#     SQL
+
+# ActiveRecord::Base.connection.execute(sql)
+
+class Dog < ActiveRecord::Base
 end
